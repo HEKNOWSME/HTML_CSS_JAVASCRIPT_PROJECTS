8648e0fc2c74958b116cd19f90ccdb1b2a7f7764
HEKNOWSME <iranziclaude@hotmail.com>
2024-10-28 11:50:12 +0200

    adding question 1

--- quizApp/js/main.js
+++ quizApp/js/main.js
@@ -1,14 +1,59 @@
-class Question {
-   constructor(question, answer) {
-      this.question = question
+class Answer {
+   constructor(answer, isCorrect) {
       this.answer = answer
+      this.isCorrect = isCorrect
    }
 }
-const Q1 = "what is the largest city"
-const ans1 = [
-   { answer: "Kigali", correct: true },
-   { answer: "Rubavu", correct: false },
-   { answer: "Rwamagana", correct: false },
-   { answer: "Rurindo", correct: false },
-]
-const question1 = new Question(Q1, ans1)
+class QuizQuestion {
+   constructor(question, answers) {
+      this.question = question
+      this.answers = answers
+   }
+}
+const answersOfQuestion1 = [new Answer('kigali', false), new Answer('Rwanda', false), new Answer('Uganda', true), new Answer('America', false)];
+const question1 = new QuizQuestion("What is the largest city in Africa", answersOfQuestion1)
+
+const answersOfQuestion2 = [new Answer('Claude', false), new Answer('iranzi', false), new Answer('Paul', true), new Answer('peter', false)];
+
+const question2 = new QuizQuestion("What is The president Of Rwanda? ", answersOfQuestion2);
+
+const answersOfQuestion3 = [new Answer('Olivier', false), new Answer('Patrick', false), new Answer('Dr Kayijuka', true), new Answer('peter', false)];
+
+const question3 = new QuizQuestion("Who is the teacher of statistic? ", answersOfQuestion3);
+
+const answersOfQuestion4 = [new Answer('Province', false), new Answer('Village', false), new Answer('District', true), new Answer('cell', false)];
+
+const question4 = new QuizQuestion("Where do we start from country?", answersOfQuestion4);
+const question = document.getElementById('question');
+const answers = document.getElementById('answers');
+const nextQuestion = document.getElementById('next-btn');
+const questions = [question1, question2, question3, question4];
+
+let currentQuestionIndex = 0;
+let currentScore = 0;
+
+function startQuiz() {
+   currentQuestionIndex = 0;
+   currentScore = 0;
+   nextQuestion.innerHTML = "next";
+   showQuestion();
+}
+
+function showQuestion() {
+   removeNext()
+   const currentQuestion = questions[currentQuestionIndex];
+   question.innerHTML = `Q ${currentQuestionIndex + 1}. ${currentQuestion.question}`
+   currentQuestion.answers.forEach(element => {
+      const btn = document.createElement('button');
+      btn.classList.add('btn');
+      btn.innerHTML = element.answer;
+      answers.appendChild(btn)
+   });
+   
+}
+
+function removeNext() {
+   nextQuestion.style.display = "none";
+}
+startQuiz()
+
