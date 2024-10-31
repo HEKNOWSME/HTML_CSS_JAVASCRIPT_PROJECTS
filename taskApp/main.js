52c111a20fd41ea91b0bacfc2c99ed6ebb7a35b3
HEKNOWSME <iranziclaude@hotmail.com>
2024-10-31 20:31:49 +0200

    add sort, delete function

--- taskApp/main.js
+++ taskApp/main.js
@@ -1,30 +1,55 @@
 let tasks = []
 const tasks_container = document.querySelector('.tasks-container');
-const input = document.getElementById('add')
-
+const input = document.getElementById('add');
 class Task {
    constructor(taskName, date) {
       this.taskName = taskName;
       this.date = date;
    }
 }
-const task = new Task();
-class ToDo {
+class ToTask {
    addTask() {
-      const taskName = input.value.trim();
-      task.taskName = taskName;
-      task.date = {
-         default: Date.now()
-      }
+      const task = new Task();
+      const value = input.value.trim();
+      if (value === "") return alert('please add something')
+      task.taskName = value;
+      task.date = new Date();
       tasks.push(task)
-      localStorage.setItem('tasks', JSON.stringify(tasks))
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      input.value = ""
    }
-   displayTask() {
-      tasks_container.innerHTML = ''
+   displayData() {
+      tasks_container.innerHTML = ""
       tasks.forEach((task, index) => {
          const article = document.createElement('article');
-         article.innerHTML = index + task.taskName ;
-         tasks_container.appendChild(article)
+         article.innerHTML = `
+         <span>${task.taskName} ${task.date}</span>
+         <button onclick="toTask.delete(${index})">Delete</button>`;
+         tasks_container.appendChild(article);
       })
    }
+   delete(index) {
+      tasks.splice(index, 1);
+      this.displayData()
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+   }
+   reverse() {
+      const data = JSON.parse(localStorage.getItem('tasks'));
+      const reversed = data.reverse();
+      tasks = reversed
+      this.displayData()
+   }
 }
+const toTask = new ToTask();
+input.addEventListener('keypress', (e) => {
+   if (e.key == 'Enter') {
+      toTask.addTask()
+      toTask.displayData()
+   }
+})
+
+window.addEventListener('load', () => {
+   const data = localStorage.getItem('tasks');
+   tasks = JSON.parse(data);
+   toTask.displayData()
+})
