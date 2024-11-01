fcdc730e5779f908b15a18bccdebb5d3c20e9c1b
HEKNOWSME <iranziclaude@hotmail.com>
2024-11-01 20:30:29 +0200

    adding search functionarity

--- taskApp/main.js
+++ taskApp/main.js
@@ -1,4 +1,5 @@
 const addInput = document.getElementById('add');
+const search = document.getElementById('search');
 const task_container = document.querySelector('.tasks-container');
 const task_number = document.querySelector('.tasks_number');
 const complete = document.querySelector('.complete')
@@ -14,15 +15,15 @@ class Task {
 class KeepNote {
    addNote() {
       if (addInput !== '') {
-      const task = new Task();
-      task.text = addInput.value;
-      task.isCompleted = false;
-      tasks.push(task);
-      this.displayNotes()   
-      number_of_task++
-      task_number.innerHTML = `${number_of_task}`;
-      localStorage.setItem('tasks', JSON.stringify(tasks));
-      localStorage.setItem('number', number_of_task);
+         const task = new Task();
+         task.text = addInput.value;
+         task.isCompleted = false;
+         tasks.push(task);
+         this.displayNotes()
+         number_of_task++
+         task_number.innerHTML = `${number_of_task}`;
+         localStorage.setItem('tasks', JSON.stringify(tasks));
+         localStorage.setItem('number', number_of_task);
       }
       complete.innerHTML = number_of_completed_task
       addInput.value = ''
@@ -49,10 +50,16 @@ class KeepNote {
          number_of_task--
          complete.innerHTML = number_of_completed_task;
          task_number.innerHTML = number_of_task;
-         localStorage.setItem('tasks', JSON.stringify(tasks));
-         localStorage.setItem('number', number_of_task);
-         localStorage.setItem('numbers', number_of_completed_task);
+      } else {
+         task.isCompleted = false;
+         number_of_completed_task--;
+         number_of_task++;
+         complete.innerHTML = number_of_completed_task;
+         task_number.innerHTML = number_of_task
       }
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      localStorage.setItem('number', number_of_task);
+      localStorage.setItem('numbers', number_of_completed_task);
    }
    delete(index) {
       const completed = tasks[index]
@@ -71,12 +78,38 @@ class KeepNote {
       }
       localStorage.setItem('tasks', JSON.stringify(tasks))
    }
+   reverse() {
+      tasks = tasks.reverse();
+      this.displayNotes()
+   }
+   search(text) {
+      tasks.forEach((item, index) => {
+         if (item.text.includes(text)) {
+         task_container.innerHTML = ''
+         const article = document.createElement('article');
+         article.innerHTML = `
+         <span>${item.text}</span>
+         <button class="not" onclick="keepNote.completed(${index})">Mark complete</button>
+         <button onclick="keepNote.delete(${index})">Delete note</button>
+         `
+            task_container.appendChild(article)
+         }
+         
+      })
+   }
 }
 
 const keepNote = new KeepNote();
 addInput.addEventListener('keypress', (e) => {
    if (e.key == "Enter") {
       keepNote.addNote();
+   }
+})
+search.addEventListener('input', () => {
+   if (search.value !== '') {
+      keepNote.search(search.value.trim())
+   } else {
+      keepNote.displayNotes()
    }
 })
 window.addEventListener('load', () => {
