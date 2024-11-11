8184f77f3d96111aa75c4916de28c41dd88cdb15
HEKNOWSME <iranziclaude@hotmail.com>
2024-11-11 23:14:01 +0200

    add input icon and time for js

--- GoogleToDoApp/js/main.js
+++ GoogleToDoApp/js/main.js
@@ -1,7 +1,11 @@
 const task = document.querySelector('.task');
 const todo_app  = document.querySelector('.todo-app')
+const add_icon = document.querySelector('.add-icon-container');
+const add_task = document.querySelector('.add-task')
 todo_app.addEventListener('click', toggle)
-
+add_icon.addEventListener('click', () => {
+   add_task.classList.toggle('show-task-input')
+})
 function toggle(e){
    const { target } = e;
    if (target.matches('.task')) {
