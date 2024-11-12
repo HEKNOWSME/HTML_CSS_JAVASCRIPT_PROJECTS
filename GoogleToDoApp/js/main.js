3a236925c43a93222031a61d6346deb8d339fa00
HEKNOWSME <iranziclaude@hotmail.com>
2024-11-12 16:31:49 +0200

    restructure codes

--- GoogleToDoApp/js/main.js
+++ GoogleToDoApp/js/main.js
@@ -1,10 +1,5 @@
-const task = document.querySelector('.task');
 const todoApp  = document.querySelector('.todo-app')
-const addIcon = document.querySelector('.add-icon-container');
 const addTask = document.querySelector('.add-task');
-const form_input = document.getElementById('task');
-const select = document.querySelector('.select');
-const submit = document.getElementById('submit')
 todoApp.addEventListener('click', toggle);
 addIcon.addEventListener('click', () => {
    addTask.classList.toggle('show-task-input')
