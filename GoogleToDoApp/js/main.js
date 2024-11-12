af32ec2e11e59f27b62386bcd87eb37788a388b5
HEKNOWSME <iranziclaude@hotmail.com>
2024-11-12 22:23:29 +0200

    add function to check form inpus

--- GoogleToDoApp/js/main.js
+++ GoogleToDoApp/js/main.js
@@ -1,8 +1,15 @@
 const todoApp  = document.querySelector('.todo-app')
 const addTask = document.querySelector('.add-task');
+const showCategory = document.querySelector('.show-category');
+const categoryContent = document.querySelector('.category-content');
+const addOneBtn = document.getElementById('add-one');
+const input = document.getElementById('input');
 todoApp.addEventListener('click', toggle);
-addIcon.addEventListener('click', () => {
-   addTask.classList.toggle('show-task-input')
+addOneBtn.addEventListener('click', checkForm);
+input.addEventListener('keypress', (e) => {
+   if (e.key === "Enter") {
+      checkForm()
+   }
 })
 function toggle(e){
    const { target } = e;
@@ -11,5 +18,27 @@ function toggle(e){
    }
    else if (target.matches('.back')) {
       todoApp.classList.toggle('show-tasks')
+   } else if (target.matches('.add')) {
+      showCategory.classList.add('show-input')
    }
+}
+
+function checkForm() {
+   const addTaskForm = addOneBtn.parentElement
+   const input = addTaskForm.querySelector('#input');
+   if (!input.value.trim()) {
+      window.alert('please enter anything')
+      return
+   }
+   const task = input.value;
+   const article = document.createElement('article');
+   article.className = "category-task"
+   article.innerHTML = `
+   <div class="input-group">
+      <input type="checkbox" name="checkbox" id="checkbox">
+      <label class="checkbox" for="checkbox">${task}</label>
+   </div>
+   <span class="span"></span>`
+   categoryContent.appendChild(article)
+   input.value = ''
 }
