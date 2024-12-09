4fc0ffb97b4cb8b1cec3022eec89e7c7362698fc
HEKNOWSME <iranziclaude@hotmail.com>
2024-12-09 16:39:48 +0200

    Adding transition to dropdown

--- dropDownList/js/main.js
+++ dropDownList/js/main.js
@@ -1,5 +1,12 @@
-const sub_nav = document.querySelector('.sub-container')
-document.querySelector('.toggle').addEventListener('click', toggle)
-function toggle() {
-   sub_nav.classList.toggle('collapse')
+const sub_nav = document.querySelector('.sub-container');
+const body = document.querySelector('body');
+body.addEventListener("click", toggle)
+function toggle(e) {
+   const { target } = e;
+   if (target.matches(".toggle")) {
+      sub_nav.classList.toggle('expanded');
+   }
+   else {
+      sub_nav.classList.remove("expanded")
+   }
 }
