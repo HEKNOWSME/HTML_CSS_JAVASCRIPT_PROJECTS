530c583fdd293417e3d05a35b85fe46f7a02b011
HEKNOWSME <iranziclaude@hotmail.com>
2024-12-09 16:27:39 +0200

    Finish project with navigation bar

--- navigation/main.js
+++ navigation/main.js
@@ -1,2 +1,11 @@
-const menu = document.querySelector('.menu');
-menu.addEventListener('click',() => menu.classList.toggle('active'))
+const body = document.querySelector('body');
+const menu = document.querySelector(".menu");
+
+body.addEventListener("click", (e) => {
+	const {target} = e
+	if (target.matches(".menu-item") || target.matches(".menu")) {
+		body.classList.toggle("toggle")
+	}else {
+		body.classList.remove("toggle")
+	}
+})
