2e665947528f7de3f901e9f21baaad274dacea74
HEKNOWSME <iranziclaude@hotmail.com>
2024-12-18 13:24:08 +0200

    Add copy Paste otp numbers

--- otps/main.js
+++ otps/main.js
@@ -1,6 +1,14 @@
 const inputs = document.querySelectorAll('.input-item')
 const btn = document.querySelector('.btn')
+const container = document.querySelector('.container')
 const message = document.querySelector('.message');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+
 btn.disabled = true
 Array.from(inputs).forEach(input => {
    input.addEventListener("keydown", (e) => {
@@ -26,3 +34,15 @@ Array.from(inputs).forEach(input => {
      
    })
 })
+window.addEventListener("paste", (e) => {
+   let numbers = parseInt(e.clipboardData.getData('text/plain'));
+   numbers = numbers.toString().split('').map(Number);
+   one.value = numbers[0]
+   two.value = numbers[1]
+   three.value = numbers[2]
+   four.value = numbers[3]
+   five.value = numbers[4]
+   six.value = numbers[5]
+   btn.disabled = false
+   console.log(numbers);
+   })
